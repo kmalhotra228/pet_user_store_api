af35c0b5b41419778e7471a6205b3788cc64519d
Kashish <kmalhotra228@gmail.com>
2025-07-28 21:41:52 +0530

    first commit

---
+++ Documents/Kashish Workspace/src/test/java/endpoints/Routes.java
@@ -0,0 +1,15 @@
+package endpoints;
+
+public class Routes {
+	
+	public static String base_url = "https://petstore.swagger.io/v2";
+	
+	//User module 
+	
+	public static String post_url_user = base_url + "/user";
+	public static String get_url_user = base_url + "/user/{username}";
+	public static String update_url_user = base_url + "/user/{username}";
+	public static String delete_url_user = base_url + "/user/{username}";
+	
+
+}
