a92c4cd432114578de097c827ba0db62d66568b9
Kashish <kmalhotra228@gmail.com>
2025-08-04 00:06:47 +0530

    changes to imprive extent report

--- src/test/java/tests/DdUserTest.java
+++ src/test/java/tests/DdUserTest.java
@@ -1,38 +1,89 @@
 
 package tests;
 
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+import java.lang.reflect.Method;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import endpoints.UserEndpoints;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import endpoints.UserEndpoints2;
 import io.restassured.response.Response;
 import payloads.User;
 import payloads.UserPayload;
 import utilties.DataProviders;
+import utilties.ExtentManager;
 
 public class DdUserTest {
 	
+	public Logger logger;
+	public Gson gson;
 	
-	
-	@Test(priority = 1,dataProvider = "datas",dataProviderClass = DataProviders.class)
-	public void testPostUser(String userID, String userName, String fName,String lName, String useremail, String pwd,String phoneNumber )
-	{
-		User userpayload = UserPayload.setUserPayload(userID, userName, fName, lName, useremail, pwd, phoneNumber);
+	@BeforeClass
+	public void setUp() {
 		
-		Response response = UserEndpoints.createUser(userpayload);
-		response.then().log().all();
+		logger = LogManager.getLogger(this.getClass());
+		gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		logger.info("Before setUp is done logger & gson object is intialised");
+	}
+	
+	@BeforeMethod
+	public void beforeTestMethod(Method method) {
+		String testName = method.getName();
+		String testDescription = method.getAnnotation(Test.class).description();
+		ExtentManager.createTest(testName,testDescription);
+	}
+	
+	
+	
+	@Test(priority = 1,dataProvider = "datas",dataProviderClass = DataProviders.class, description = "create a user with valid payload")
+	public void testPostUser(String userID, String userName, String fName,String lName, String useremail, String pwd,String phoneNumber )
+	
+	{
+		logger.info("Create a new user is intialised...");
+		User userpayload = UserPayload.setUserPayload(userID, userName, fName, lName, useremail, pwd, phoneNumber);
+		logger.info("User payload is created");
+		logger.info(gson.toJson(userpayload));
+		ExtentManager.logInfo("Create a New User");
+		ExtentManager.logApiRequest(gson.toJson(userpayload));
+		
+		Response response = UserEndpoints2.createUser(userpayload, logger);
+		response.then().body(matchesJsonSchemaInClasspath("schemas/user_post_response_schema.json"));
 		
 		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		ExtentManager.logApiResponse(response.asPrettyString());
+		ExtentManager.logApiTest("/user", "POST", gson.toJson(userpayload), response.asPrettyString(), response.getStatusCode());
+		
+		logger.info("create user is completed");
 	}
 	
 	@Test(priority = 2,dataProvider = "usernames",dataProviderClass = DataProviders.class)
 	public void testDeleteUserByName(String username)
+	
 	{
-		Response response = UserEndpoints.deleteUser(username);
+		logger.info("delete user is intialised...");
+		ExtentManager.logInfo("Delete a User");
+		Response response = UserEndpoints2.deleteUser(username,logger);
+		ExtentManager.logApiRequest(gson.toJson(username));
 		
-		response.then().log().all();
+		response.then().body(matchesJsonSchemaInClasspath("schemas/user_delete_response_schema.json"));
 		
 		Assert.assertEquals(response.getStatusCode(), 200);
+		ExtentManager.logApiResponse(response.asPrettyString());
+		ExtentManager.logApiTest("/user/"+username, "DELETE", username, response.asPrettyString(), response.getStatusCode());
+		logger.info("delete user is completed");
 	}
 
 }
--- src/test/java/tests/userTest2.java
+++ src/test/java/tests/userTest2.java
@@ -6,6 +6,9 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import endpoints.UserEndpoints2;
 import io.restassured.response.Response;
 import payloads.User;
@@ -20,23 +23,25 @@ public class userTest2 {
     Faker faker;
     User userPayload;
     public Logger logger;
+    public Gson gson;
 
     @BeforeClass
     public void setUpData() {
         this.userPayload = UserPayload.setUserPayload();
         logger = LogManager.getLogger(this.getClass());
+        gson = new GsonBuilder().setPrettyPrinting().create();
     }
 
     @Test(priority = 1)
     public void testPostUser() {
     	logger.info("Creating new user...");
         ExtentManager.logInfo("Creating new user...");
-        ExtentManager.logApiRequest(this.userPayload.toString());
+        ExtentManager.logApiRequest(gson.toJson(this.userPayload));
 
         Response response = UserEndpoints2.createUser(this.userPayload,logger);
         ExtentManager.logApiResponse(response.asPrettyString());
 
-        ExtentManager.logApiTest("/user", "POST", this.userPayload.toString(), response.asString(), response.getStatusCode());
+        ExtentManager.logApiTest("/user", "POST", gson.toJson(this.userPayload), response.asString(), response.getStatusCode());
         response.then().body(matchesJsonSchemaInClasspath("schemas/user_post_response_schema.json"));
         Assert.assertEquals(response.getStatusCode(), 200);
         Assert.assertEquals(Integer.parseInt(response.jsonPath().get("message")), this.userPayload.getId());
@@ -49,11 +54,13 @@ public class userTest2 {
     
     	logger.info("GET new user details");
         ExtentManager.logInfo("Retrieving user: " + this.userPayload.getUsername());
+        ExtentManager.logApiRequest(this.userPayload.getUsername());
 
         Response response = UserEndpoints2.readUser(this.userPayload.getUsername(),logger);
         ExtentManager.logApiResponse(response.asPrettyString());
 
         ExtentManager.logApiTest("/user/" + this.userPayload.getUsername(), "GET", "", response.asString(), response.getStatusCode());
+        response.then().body(matchesJsonSchemaInClasspath("schemas/user_get_response_schema.json"));
         Assert.assertEquals(response.getStatusCode(), 200);
         
         logger.info("New user details is sucessfully fetched");
@@ -75,14 +82,21 @@ public class userTest2 {
         logger.info("After update fname:"+ this.userPayload.getFirstName());
         logger.info("After update lname:"+ this.userPayload.getLastName());
         logger.info("After update fname:"+ this.userPayload.getEmail());
+        
+        ExtentManager.logApiRequest(gson.toJson(updatedPayload));
+        
+        
 
         Response response = UserEndpoints2.updateUser(username, this.userPayload,logger);
-        ExtentManager.logApiTest("/user/" + username, "PUT", updatedPayload.toString(), response.asString(), response.getStatusCode());
+        ExtentManager.logApiTest("/user/" + username, "PUT", gson.toJson(updatedPayload), response.asString(), response.getStatusCode());
+        ExtentManager.logApiResponse(response.asString());
+        response.then().body(matchesJsonSchemaInClasspath("schemas/user_put_response_schema.json"));
         Assert.assertEquals(response.getStatusCode(), 200);
 
         // Verify updated user
-        Response responseAfterUpdate = UserEndpoints2.readUser(updatedPayload.getUsername(),logger);
-        ExtentManager.logApiTest("/user/" + updatedPayload.getUsername(), "GET", "", responseAfterUpdate.asString(), responseAfterUpdate.getStatusCode());
+        Response responseAfterUpdate = UserEndpoints2.readUser(this.userPayload.getUsername(),logger);
+        ExtentManager.logApiTest("/user/" + this.userPayload.getUsername(), "GET", "", responseAfterUpdate.asString(), responseAfterUpdate.getStatusCode());
+        responseAfterUpdate.then().body(matchesJsonSchemaInClasspath("schemas/user_get_response_schema.json"));
         Assert.assertEquals(responseAfterUpdate.getStatusCode(), 200);
         
         logger.info("Sucessfully updated the user: "+ this.userPayload.getUsername());
@@ -92,9 +106,12 @@ public class userTest2 {
     public void testDeleteUserByName() {
     	logger.info("Deleting the user");
         ExtentManager.logInfo("Deleting user: " + this.userPayload.getUsername());
+        ExtentManager.logApiRequest(this.userPayload.getUsername());
 
         Response response = UserEndpoints2.deleteUser(this.userPayload.getUsername(),logger);
+        ExtentManager.logApiResponse(response.asString());
         ExtentManager.logApiTest("/user/" + this.userPayload.getUsername(), "DELETE", "", response.asString(), response.getStatusCode());
+        response.then().body(matchesJsonSchemaInClasspath("schemas/user_delete_response_schema.json"));
         Assert.assertEquals(response.getStatusCode(), 200);
         logger.info("new user is sucessfully deleted");
     }
